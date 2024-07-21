792d2cccb9defe794cef49a4182ea3818e32b243
thilinam <thilinamanawadu@gmail.com>
2024-07-21 10:29:57 +0530

    # repository updates

--- src/main/java/com/thilina/booking_manager/repository/BookRecordRepository.java
+++ src/main/java/com/thilina/booking_manager/repository/BookRecordRepository.java
@@ -1,9 +1,13 @@
 package com.thilina.booking_manager.repository;
 
+import com.thilina.booking_manager.entity.Book;
 import com.thilina.booking_manager.entity.BookRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRecordRepository extends JpaRepository<BookRecord,String> {
+    Optional<BookRecord> findByBookIdAndBookBorrowDateIsNotNullAndBookReturnDateIsNull(Book book);
 }
