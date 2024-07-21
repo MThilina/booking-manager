8f602857eb4385f26ab8e6478342c27a9d49b7d2
thilinam <thilinamanawadu@gmail.com>
2024-07-21 10:41:27 +0530

    # Exception Update

--- src/main/java/com/thilina/booking_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thilina/booking_manager/exception/GlobalExceptionHandler.java
@@ -16,14 +16,14 @@ import java.time.Instant;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<ErrorResponseDto> handleBadRequestException(RuntimeException ex) {
+    protected ResponseEntity<ErrorResponseDto> handleBadRequestException(BadRequestException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(),HttpStatus.BAD_REQUEST.value(), Instant.now());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(NotfoundException.class)
-    protected ResponseEntity<ErrorResponseDto> handleNotFoundException(RuntimeException ex) {
+    protected ResponseEntity<ErrorResponseDto> handleNotFoundException(NotfoundException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(),HttpStatus.NOT_FOUND.value(), Instant.now());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
