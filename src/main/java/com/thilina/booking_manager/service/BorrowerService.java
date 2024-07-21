a4e20d68883f2f2ced539a908bcd6705f2b11991
thilinam <thilinamanawadu@gmail.com>
2024-07-21 12:38:35 +0530

    # completed main logic and transactional handeling

--- src/main/java/com/thilina/booking_manager/service/BorrowerService.java
+++ src/main/java/com/thilina/booking_manager/service/BorrowerService.java
@@ -12,8 +12,12 @@ import com.thilina.booking_manager.mappers.BorrowerMapper;
 import com.thilina.booking_manager.repository.BookRecordRepository;
 import com.thilina.booking_manager.repository.BookRepository;
 import com.thilina.booking_manager.repository.BorrowerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -39,6 +43,7 @@ public class BorrowerService {
         return BorrowerMapper.INSTANCE.toDTO(borrower);
     }
 
+    @Transactional(value = Transactional.TxType.REQUIRED,rollbackOn =Exception.class)
     public BookDto borrowBook(String borrowerId, String bookId) {
         // Validate user and book
         Borrower borrower = borrowerRepository.findById(borrowerId)
@@ -53,13 +58,12 @@ public class BorrowerService {
         if (optionalRecord.isPresent()) {
             record = optionalRecord.get();
 
-            // If the record exists, check the borrow and return dates
+            // If the record exists, check the borrow and return dates (execute validations)
             this.validateBookRecord(record);
         } else {
             // Create a new BookRecord if it doesn't exist
             record = this.createNewBookRecord(book, borrower);
         }
-
         // Set the default return date and save the record
         record.setBookReturnDate(null); // Assuming the book is now being borrowed, so reset return date
         recordRepository.save(record);
@@ -68,6 +72,7 @@ public class BorrowerService {
     }
 
 
+    @Transactional(value = Transactional.TxType.REQUIRED,rollbackOn =Exception.class)
     public BookDto returnBook(String borrowerId, String bookId) {
         Borrower borrower = borrowerRepository.findById(borrowerId)
                 .orElseThrow(() -> new NotfoundException("Borrower not found"));
@@ -75,19 +80,20 @@ public class BorrowerService {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new NotfoundException("Book not found"));
 
+        Optional<BookRecord> optionalRecord = recordRepository.findByBookIdAndBookBorrowDateIsNotNullAndBookReturnDateIsNull(book);
 
-        Optional<BookRecord> optionalRecord = recordRepository.findById(book.getId());
-
-        BookRecord record = optionalRecord.orElseThrow(() -> new BookRecordNotFoundException("Book distribution details cannot found."));
-
+        if(!optionalRecord.isPresent()){
+            // throw exception
+            throw new BookRecordNotFoundException("Book distribution details cannot found.");
+        }
+        BookRecord record;
+        record = optionalRecord.get();
         if (record.getBookBorrowDate() != null && record.getBookReturnDate() != null) {
             throw new BookNotReturnedException("Book has been return already ");
-        } else {
-            // else books can be borrowed
-            record.getBookReturnDate();
-            recordRepository.save(record);
         }
-
+        record.setBookReturnDate(Timestamp.from(Instant.now()).toString());
+        // else books can be borrowed
+        recordRepository.save(record);
         return BookMapper.INSTANCE.toDTO(book);
     }
 
